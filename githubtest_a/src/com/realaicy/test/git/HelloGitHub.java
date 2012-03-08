21407bfc18b6924114274eabdbf6b474e2a11fe6
realaicy <realaicy@126.com>
2012-03-08 14:47:11 +0800

    commit init local

---
+++ githubtest_a/src/com/realaicy/test/git/HelloGitHub.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.realaicy.test.git;
+
+/**
+ * @author REALAICY
+ *
+ */
+public class HelloGitHub {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		System.out.println("Hello Github I am Xudong Liu 刘旭东！");
+
+	}
+
+}
